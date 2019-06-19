44c12fdb0d25a791afca12d5b66a6e408eda3950
mchugh16 <mira.chugh@checkr.com>
2019-06-19 14:00:59 -0700

    work until test section

---
+++ flaskr/__init__.py
@@ -0,0 +1,42 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    # create instance of flask app 
+    app = Flask(__name__, instance_relative_config=True)
+
+    # configure app
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    if test_config is None:
+        # load instance config (if exists) when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load test config when testing
+        app.config.from_mapping(test_config)
+    
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    # page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    return app
